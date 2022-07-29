6c458cc82d09720d8e2ad4130ff56edaa4cdde11
Kubrianity <kubraaozerenn@gmail.com>
2022-07-29 18:21:28 +0300

    Get dataset values of all buttons

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-
+const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
@@ -32,3 +32,8 @@ function displayEndTime(timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+}
+buttons.forEach(button => button.addEventListener('click', startTimer))
