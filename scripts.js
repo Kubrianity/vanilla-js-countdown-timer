a28f49428e963bdde8cb4b6fb0a36c9ef71456a5
Kubrianity <kubraaozerenn@gmail.com>
2022-07-29 15:07:04 +0300

    Add fn to show seconds in the beginning

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let countdown;
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         //check if we should stop it
@@ -10,6 +11,10 @@ function timer(seconds) {
             return;
         }
         //display it
-        console.log(secondsLeft);
+        displayTimeLeft(secondsLeft);
     }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+    console.log(seconds);
 }
