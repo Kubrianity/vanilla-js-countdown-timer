85f9cfbef3c08703e4e0d2c6f77cc2e209980ced
Kubrianity <kubraaozerenn@gmail.com>
2022-07-29 18:13:25 +0300

    Add fn to show planned time

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayEndTime(then);
     displayTimeLeft(seconds);
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -21,5 +23,12 @@ function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
     timerDisplay.textContent = display;
+}
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
