0dddf19c16ef574f545aed5a820719dcedd6161e
Kubrianity <kubraaozerenn@gmail.com>
2022-07-29 14:56:19 +0300

    Stop interval when secondsLeft is zero

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
+let countdown;
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
-        console.log(secondsLeft)
+        //check if we should stop it
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        //display it
+        console.log(secondsLeft);
     }, 1000)
 }
